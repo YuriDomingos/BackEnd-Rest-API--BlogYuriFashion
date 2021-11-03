6e17a19ba6048e6b72269fbd1b4276e8a6f4abbd
YuriDomingos <josemarlabareda@gmail.com>
2021-11-03 08:12:48 +0100

    code

--- Server/Routes/posts.js
+++ Server/Routes/posts.js
@@ -84,9 +84,8 @@ router.get("/:id", async (req, res)=>{
 
     try {
            
-         const user = await User.findById(req.params.id);
-         const {password, ...others} = user._doc;
-         res.status(200).json(others);
+         const post = await Post.findById(req.params.id);
+         res.status(200).json(post);
           
     }catch(err){
 
@@ -109,7 +108,7 @@ router.post("/", async (req, res) => {
     }catch(err)
     { 
         res.status(500).json(err);
-        console.log("Não deu para criar o poat ")
+       
     }
 })
 
